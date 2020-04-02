393f2d35bf69a56980034d75415300ba36125c45
Abdulaziz Sayed <36112643+Abdulazizsayed@users.noreply.github.com>
2020-04-02 23:58:08 +0200

    Resolving some mistakes
    Some additional spaces removed

--- SoftwareProject/models/Connection.php
+++ SoftwareProject/models/Connection.php
@@ -4,11 +4,7 @@ class Connection {
     static $conn;
 
     // DB connect
-    public static function connect($host = 'mysql5019.site4now.net
-', $dbName = 'db_a57458_sw2
-', $username = 'a57458_sw2
-', $password = 'SW123456789
-'){
+    public static function connect($host = 'mysql5019.site4now.net', $dbName = 'db_a57458_sw2', $username = 'a57458_sw2', $password = 'SW123456789'){
         Connection::$conn = null;
         try{
             Connection::$conn = new PDO('mysql:host=' . $host . ';dbname=' . $dbName, $username, $password);
