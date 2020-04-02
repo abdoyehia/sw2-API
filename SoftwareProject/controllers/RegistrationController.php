4d3bbc4887ad776d7bf15e7c9350852f6b1f7f4f
abdonasef733@gmail.com <61522713+HamidoNasef@users.noreply.github.com>
2020-04-02 20:33:40 +0200

    Controllers
    User Controller
    Registration Controller
    Admin Controller

---
+++ SoftwareProject/controllers/RegistrationController.php
@@ -0,0 +1,25 @@
+<?php 
+include_once 'models/NormalUser.php';
+include_once 'models/StoreOwner.php';
+include_once 'models/UserInfo.php';
+
+class RegistrationController {
+
+	public function signUp($username, $password, $email, $type) {
+		if(UserInfo::getUser($username)->rowCount() > 0) {
+			return false;
+		} else {
+			$user = new UserInfo();
+			$user->username = $username;
+			$user->pass = $password;
+			$user->email = $email;
+			$user->type = $type;
+
+			$user->save();
+			return true;
+		}
+	}
+	
+}
+
+?>
---
+++ SoftwareProject/controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php 
+include '../models/NormalUser.php';
+include '../models/StoreOwner.php';
+include '../models/Admin.php';
+
+class UserController {
+	public function login($usernameOrEmail, $password) {
+
+	}
+}
+
+?>
